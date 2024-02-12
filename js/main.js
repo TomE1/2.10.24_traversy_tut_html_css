f179bccd4245274c313aa5cc6015b6d94891d2b4
Tom Evans <35412739+TomE1@users.noreply.github.com>
2024-02-12 12:39:33 +0700

    media queries

--- js/main.js
+++ js/main.js
@@ -1,10 +1,10 @@
 // FAQ Accordion
-
 document.addEventListener("DOMContentLoaded", () => {
   const faqContainer = document.querySelector(".faq-content");
 
   faqContainer.addEventListener("click", (e) => {
-    const groupHeader = e.target.closest("faq-group-header");
+    const groupHeader = e.target.closest(".faq-group-header");
+
     if (!groupHeader) return;
 
     const group = groupHeader.parentElement;
@@ -14,5 +14,22 @@ document.addEventListener("DOMContentLoaded", () => {
     // Toggle icon
     icon.classList.toggle("fa-plus");
     icon.classList.toggle("fa-minus");
+
+    // Toggle visibility of body
+    groupBody.classList.toggle("open");
+
+    // Close other open FAQ bodies
+    const otherGroups = faqContainer.querySelectorAll(".faq-group");
+
+    otherGroups.forEach((otherGroup) => {
+      if (otherGroup !== group) {
+        const otherGroupBody = otherGroup.querySelector(".faq-group-body");
+        const otherIcon = otherGroup.querySelector(".faq-group-header i");
+
+        otherGroupBody.classList.remove("open");
+        otherIcon.classList.remove("fa-minus");
+        otherIcon.classList.add("fa-plus");
+      }
+    });
   });
 });
